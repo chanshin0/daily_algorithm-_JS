f65d814b427ff4b84a322b3e1c04b5747c6eefe2
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-03-27 00:42:47 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,11 +1,11 @@
 function solution(s1, s2) {
     var answer = 0;
-    s1.forEach(ele=>{
-        if (s2.includes(ele)) answer ++
-    })
+    // s1.forEach(ele=>{
+    //     if (s2.includes(ele)) answer ++
+    // })
     
-    // const wholeNumbers = [...s1,...s2]
-    // const setNumbers = new Set(wholeNumbers)
-    // answer = wholeNumbers.length - [...setNumbers].length
+    const wholeNumbers = [...s1,...s2]
+    const setNumbers = new Set(wholeNumbers)
+    answer = wholeNumbers.length - [...setNumbers].length
     return answer;
 }
