a83cb195567cfc4f29104ed54f8283279629d597
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-03-27 00:30:12 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.js
@@ -0,0 +1,9 @@
+function solution(numbers, num1, num2) {
+    return numbers.slice(num1, num2+1);
+    // let answer = []
+    // for (let i=0; i<numbers.length; i++){
+    //     const num = numbers[i]
+    //     if (num1<=i && i<=num2) answer.push(num)
+    // }
+    // return answer
+}
