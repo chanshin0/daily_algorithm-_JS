f6859245a95a1042476f49df4a8b319d097e4962
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-03-27 00:35:11 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = [0,0];
+    
+    num_list.forEach(num=>{
+        if (num%2===0) answer[0] += 1
+        else answer[1] += 1
+    })
+    
+    return answer;
+}
