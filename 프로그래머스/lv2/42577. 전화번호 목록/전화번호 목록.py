c41a0b2d4b2a1015c0c52078acc8375b4602b491
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-03-28 01:38:36 +0900

    [level 2] Title: 전화번호 목록, Time: 221.17 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,8 @@
 def solution(phone_book):
-    phone_map = {}
-    
-    # Create a hash map with phone numbers as keys and set all values to 1
-    for phone in phone_book:
-        phone_map[phone] = 1
-    
-    # Check if any phone number is a prefix of another
-    for phone in phone_book:
-        for i in range(1, len(phone)):
-            if phone[:i] in phone_map:
-                return False
+
+    phone_book.sort(key=lambda x:(x,len(x)))
+    for i in range(len(phone_book)-1):
+        l = len(phone_book[i])
+        if phone_book[i] == phone_book[i+1][:l]:
+            return False
     return True
