d9ebb9fcf2aae77767c23df008418446f110ab53
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-03-29 23:30:13 +0900

    [level 0] Title: A로 B 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -0,0 +1,23 @@
+function solution(before, after) {
+    // [1]
+    let beforeObj = {}
+    let afterObj = {}
+    
+    for (let i=0; i<before.length; i++){
+        const be = before[i]
+        const af = after[i]
+        if (beforeObj[be]) beforeObj[be] += 1
+        else beforeObj[be] = 1
+        if (afterObj[af]) afterObj[af] += 1
+        else afterObj[af] = 1
+    }
+    // [2]
+    for (key in beforeObj){
+        if (afterObj.hasOwnProperty(key) && beforeObj[key]===afterObj[key]) continue
+        else return 0
+    }
+    return 1
+}
+
+// 객체에 각각 저장
+// [키, 밸류]를 원소로 갖는 배열로 만들어 비교.
