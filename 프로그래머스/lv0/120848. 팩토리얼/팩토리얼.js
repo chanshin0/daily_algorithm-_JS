58aaa23d446a5e183d257362842db238fbba2c71
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-03-30 00:01:49 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,9 +1,19 @@
+const factorial = (n)=>{
+    if (n<=1) return 1
+    else return n*factorial(n-1)
+}
+
+const memo = []
+for (let i=1; i<=10; i++){
+    const facto = factorial(i)
+    memo.push(facto)
+}
+
 function solution(n) {
-    let k = 1;
-    for(let i =1; i<12; i++){
-        k *= i;
-        if(k>n){
-            return i - 1
-        }
+
+    for (let i=0; i<=9; i++){
+        if (memo[i]>n) return i 
+        else if (memo[i]===n) return i+1
     }
+
 }
