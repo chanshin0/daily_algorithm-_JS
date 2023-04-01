9477bb7efa3be83a33f4c66e14ed413f634332ae
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-04-01 20:41:35 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -3,16 +3,22 @@ function solution(my_string) {
     
     my_string = my_string.split(' ')
     
-    for (let i=0; i<my_string.length; i++){
+    for (let i=0; i<my_string.length;){
         if (my_string[i]==='+'){
             answer += +my_string[i+1]
-            i ++
+            i += 2
         } else if (my_string[i]==='-'){
             answer -= +my_string[i+1]
-            i ++
+            i += 2
         }
-        else answer = +my_string[i]
+        else{
+            answer = +my_string[i]
+            i ++
+        } 
     }
     
     return answer
 }
+
+// 1. +나오면 뒷 숫자 더하기, i++해서 다음 반복문 스킵
+// 2. -나오면 뒷 숫자 빼고, i++해서 다음 반복문 스킵
