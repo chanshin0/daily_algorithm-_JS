c75789da242a84d94b0a8dded1c29c9fb4c56d7d
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-04-06 00:36:49 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -2,45 +2,48 @@ function isAlpha(str) {
   return /^[A-Za-z]+$/.test(str);
 }
 
+function createObj(str) {
+  const obj = {};
+    
+  for (let i = 0; i < str.length - 1; i++) {
+    const checkStr = (str[i] + str[i + 1]).toLowerCase();
+      
+    if (!isAlpha(checkStr)) continue;
+    obj[checkStr] = (obj[checkStr] || 0) + 1;
+      
+  }
+    
+  return obj;
+}
+
 function solution(str1, str2) {
-    var answer = 0;
-    const obj1 = {}
-    const obj2 = {}
+  let answer = 0;
 
-    for (let i=0; i<str1.length-1; i++){
-        const checkStr = (str1[i]+str1[i+1]).toLowerCase()
-        if (!isAlpha(checkStr)) continue
+  const obj1 = createObj(str1);
+  const obj2 = createObj(str2);
 
-        if (obj1[checkStr]) obj1[checkStr] += 1
-        else obj1[checkStr] = 1
+  let shared = 0;
+  let sumSet = 0;
 
+  for (const key in obj1) {
+    if (obj2[key]) {
+      shared += Math.min(obj1[key], obj2[key]);
+      sumSet += Math.max(obj1[key], obj2[key]);
+    } else {
+      sumSet += obj1[key];
     }
-    for (let i=0; i<str2.length-1; i++){
-        const checkStr = (str2[i]+str2[i+1]).toLowerCase()
-        if (!isAlpha(checkStr)) continue
-
-        if (obj2[checkStr]) obj2[checkStr] += 1
-        else obj2[checkStr] = 1
-    }    
-
-    const keys1 = Object.keys(obj1)
-    const keys2 = Object.keys(obj2)
-
-    let shared = 0
-    let sumSet = 0
-    for (key of keys1){
-        if (keys2.includes(key)){
-            shared += Math.min(obj1[key], obj2[key])
-            sumSet += Math.max(obj1[key], obj2[key])
-        }
-        else sumSet += obj1[key]
+  }
+  for (const key in obj2) {
+    if (!obj1[key]) {
+      sumSet += obj2[key];
     }
-    for (key2 of keys2){
-        if (!keys1.includes(key2)) sumSet += obj2[key2]
-    }
+  }
 
-    if (shared===0 && sumSet===0) answer = 65536
-    else answer = (shared/sumSet)*65536
+  if (shared === 0 && sumSet === 0) {
+    answer = 65536;
+  } else {
+    answer = (shared / sumSet) * 65536;
+  }
 
-    return Math.floor(answer);
+  return Math.floor(answer);
 }
