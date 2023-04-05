5b8d5d2d60a6442162833bf4809c19bbe1c9f068
chanshin0 <109333130+chanshin0@users.noreply.github.com>
2023-04-06 00:09:07 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.42 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,46 +1,27 @@
-function isAlpha(str) {
-  return /^[A-Za-z]+$/.test(str);
-}
+function solution (str1, str2) {
 
-function solution(str1, str2) {
-    var answer = 0;
-    const obj1 = {}
-    const obj2 = {}
-    
-    for (let i=0; i<str1.length-1; i++){
-        const checkStr = (str1[i]+str1[i+1]).toLowerCase()
-        if (!isAlpha(checkStr)) continue
-        
-        if (obj1[checkStr]) obj1[checkStr] += 1
-        else obj1[checkStr] = 1
-        
+  function explode(text) {
+    const result = [];
+    for (let i = 0; i < text.length - 1; i++) {
+      const node = text.substr(i, 2);
+      if (node.match(/[A-Za-z]{2}/)) {
+        result.push(node.toLowerCase());
+      }
     }
-    for (let i=0; i<str2.length-1; i++){
-        const checkStr = (str2[i]+str2[i+1]).toLowerCase()
-        if (!isAlpha(checkStr)) continue
-        
-        if (obj2[checkStr]) obj2[checkStr] += 1
-        else obj2[checkStr] = 1
-    }    
-    
-    const keys1 = Object.keys(obj1)
-    const keys2 = Object.keys(obj2)
-    
-    let shared = 0
-    let sumSet = 0
-    for (key of keys1){
-        if (keys2.includes(key)){
-            shared += Math.min(obj1[key], obj2[key])
-            sumSet += Math.max(obj1[key], obj2[key])
-        }
-        else sumSet += obj1[key]
-    }
-    for (key2 of keys2){
-        if (!keys1.includes(key2)) sumSet += obj2[key2]
-    }
-  
-    if (shared===0 && sumSet===0) answer = 65536
-    else answer = (shared/sumSet)*65536
-    
-    return Math.floor(answer);
+    return result;
+  }
+
+  const arr1 = explode(str1);
+  const arr2 = explode(str2);
+  const set = new Set([...arr1, ...arr2]);
+  let union = 0;
+  let intersection = 0;
+
+  set.forEach(item => {
+    const has1 = arr1.filter(x => x === item).length;
+    const has2 = arr2.filter(x => x === item).length;
+    union += Math.max(has1, has2);
+    intersection += Math.min(has1, has2);
+  })
+  return union === 0 ? 65536 : Math.floor(intersection / union * 65536);
 }
