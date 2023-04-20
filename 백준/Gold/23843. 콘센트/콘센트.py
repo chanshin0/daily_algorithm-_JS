b23e5274a885f668fdaee5b8af8153f011693876
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-04-20 22:11:34 +0900

    [Gold V] Title: 콘센트, Time: 128 ms, Memory: 115288 KB -BaekjoonHub

--- 백준/Gold/23843. 콘센트/콘센트.py
+++ 백준/Gold/23843. 콘센트/콘센트.py
@@ -1,37 +1,32 @@
 # 콘센트
 N, M = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
-# print(N, M)
-# print(arr)
 
 time = 0
 concent = []
 
-while True:
+# [1]. 콘센트에 전자기기를 전부 낄 때까지 걸리는 시간을 구하는 반복문.
+while arr:
+    # [1-1]. 콘센트가 꽉차거나, 전자기기가 없을 때까지 꼽음.
+    while len(concent) < M and len(arr)>0:
+        concent.append(arr.pop())
+    concent = sorted(concent, reverse=True)
 
-    while arr:
-        while len(concent) < M and len(arr)>0:
-            concent.append(arr.pop())
-        concent = sorted(concent, reverse=True)
+    # [1-2]. (정렬) 젤 뒤에있는 기기(fast)가 젤 빨리 충전 끝남. 그 시간만큼 흘렀다고 가정.
+    fast = concent[-1]
+    time += fast
 
-        # print(time, concent, arr)
+    # [1-3]. 콘센트를 뒤에서 부터 순회하면서 어디까지 충전 다 되었는지 체크
+    tagetId = -1
+    for i in range(len(concent)-1, -1, -1):
+        concent[i] -= fast
+        if concent[i] == 0:
+            tagetId = i
 
-        fast = concent[-1]
-        time += fast
+    # [1-4]. 충전 다 된 기기들 버리고 자름. 이후 1~4 반복.
+    concent = concent[:tagetId]
 
-        tagetId = -1
-        for i in range(len(concent)-1, -1, -1):
-            concent[i] -= fast
-            if concent[i] == 0:
-                tagetId = i
-
-        concent = concent[:tagetId]
-
-    if (concent):
-        arr = concent
-        concent = []
-    else:
-        break
-
+if (concent):
+    time += max(concent)
 print(time)
 
