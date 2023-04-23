ad7d23ea96a1d2030ee193f551e0fc317421d3bb
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-04-23 22:01:40 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 13.44 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,28 @@
+function solution(queue1, queue2) {
+    var answer = 0;
+    const sum1 = queue1.reduce((acc, curr)=>acc+curr, 0)
+    const sum2 = queue2.reduce((acc, curr)=>acc+curr, 0)
+    const half = (sum1+sum2)/2
+    
+    const list = [...queue1, ...queue2]
+    let left = 0;
+    let right = queue1.length-1;
+    
+    let sum = sum1
+    while (true) {
+        if (answer >= list.length*2) return -1;
+
+        if (sum===half) break;
+        else if (sum < half) {
+            right ++
+            sum += list[right]
+        }
+        else {
+            left ++
+            sum -= list[left-1]
+        }
+        answer ++
+    }
+    
+    return answer;
+}
