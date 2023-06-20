f78ba4bd9666bf2d8c50bb422a9f02ab678b2d4c
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-06-21 08:55:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -0,0 +1,25 @@
+function solution(operations) {
+    var answer = [];
+    
+    const que = []
+    for (const op of operations) {
+        const [m, n] = op.split(' ')
+        
+        if (m === 'I') {
+            que.push(+n)
+            que.sort((a,b)=>a-b)
+        } else {
+            if (que.length === 0) continue
+            
+            if (n === '1') {
+                que.pop()    
+            } else {
+                que.shift()
+            }
+        }
+    }
+
+    const l = que.length
+    
+    return l === 0 ? [0,0] : [que[l-1], que[0]];
+}
