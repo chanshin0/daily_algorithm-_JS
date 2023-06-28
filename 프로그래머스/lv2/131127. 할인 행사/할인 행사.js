fa533c376a85975fa4b52c049c8288706649ce1a
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-06-28 11:52:44 +0900

    [level 2] Title: 할인 행사, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -0,0 +1,41 @@
+function solution(want, number, discount) {
+    var answer = 0;
+    
+    const obj = {}
+    for (let i=0; i<want.length; i++) {
+        const [w, n] = [want[i], number[i]]
+        obj[w] = n
+    }
+
+    
+    for (let j=0; j<discount.length-9; j++) {
+        let item = discount[j]
+        if (!obj[item]) continue
+        
+        let cart = {}
+        let flag = true
+        for (let i=j; i<j+10; i++) {
+            item = discount[i]
+            cart[item]? cart[item] += 1 : cart[item] = 1
+            if (cart[item] > obj[item]||0) {
+                flag= false
+                break
+            }
+        }
+        
+        if (flag) {
+            let newFlag = true
+            for (const w of want) {
+                if (obj[w] !== cart[w]) {
+                    newFlag = false
+                    break
+                }
+            }
+            if (newFlag) answer ++
+        }
+        
+        
+    } 
+    
+    return answer;
+}
