35ed2167fdb8e27a9508877f39716b56b3d089f0
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-06-29 09:56:09 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.73 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -2,8 +2,8 @@ function solution(n, left, right) {
     const answer = [];
 
     for (let i = left; i <= right; i++) {
-        const row = Math.floor(i / n);
-        const col = i % n;
+        const row = Math.floor(i / n);  // 몫
+        const col = i % n;              // 나머지
         answer.push(Math.max(row, col) + 1);
     }
 
