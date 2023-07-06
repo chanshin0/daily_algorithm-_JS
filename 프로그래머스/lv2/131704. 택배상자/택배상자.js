19325ae884338fd9e0f5542223897eedcb73713d
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-07-06 11:50:38 +0900

    [level 2] Title: 택배상자, Time: 42.40 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -0,0 +1,27 @@
+function solution(order) {
+    var answer = 0;
+    const l = order.length
+    
+    let sub = Array(order[0]).fill(null).map((_,idx)=>idx+1)
+    let box = order[0] + 1
+    let idx = 0 // shift의 시간복잡도가 O(n)이기 때문에, 사용하지 않기 위해서 order배열의 idx로 계산함
+    
+    while (idx < l) {
+        // console.log(order, box, sub, answer)
+        if (box === order[idx]) {
+            box++
+            idx++
+            answer++
+        } else if (sub.at(-1) === order[idx]) {
+            sub.pop()
+            idx++
+            answer ++
+        } else {
+            if (order[idx] < box) return answer
+            sub.push(box)
+            box++
+        }
+    }
+    
+    return answer;
+}
