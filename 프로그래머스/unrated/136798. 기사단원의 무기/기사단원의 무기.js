e149eeaa232356ed06475201694e354b18bf3278
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-07-27 00:50:45 +0900

    [unrated] Title: 기사단원의 무기, Time: 15.48 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,23 @@
+function solution(number, limit, power) {
+    var answer = 0;
+    
+    const yaksu = []
+    for (let i=1; i<=number; i++) {
+        const w = find(i)
+        answer += w
+        console.log(w)
+    }
+    
+    function find (n) {
+        let set = new Set()
+        for (let i=1; i<=(n**0.5); i++) {
+            if (n%i === 0) {
+                set.add(i).add(n/i)
+            }
+            if (set.size > limit) return power
+        }
+        return set.size
+    }
+    
+    return answer;
+}
