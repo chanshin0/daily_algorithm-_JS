8480077d798f17028320f3bfc5042e3a63d61bf7
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2023-08-03 17:28:06 +0900

    [unrated] Title: 리코쳇 로봇, Time: 1.15 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.js
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -2,7 +2,7 @@ function solution(board) {
     var answer = 0;
     const [row, col] = [board.length, board[0].length]
     
-    const visited = Array.from({length:row}, ()=>Array(col).fill(row*col))
+    const visited = Array.from({length:row}, ()=>Array(col).fill(0))
     const delta = [[0,1],[1,0],[0,-1],[-1,0]]
     
     board.forEach((row, i)=>{
@@ -14,26 +14,30 @@ function solution(board) {
     })
     
     function bfs(i, j) {
-        visited[i][j] = 0
+        visited[i][j] = 1
         
-        const que = [[i, j]]
+        const que = [[i, j, 0]]
         while (que.length > 0) {
-            const [si, sj] = que.shift()
+            const [si, sj, cnt] = que.shift()
             
-            if (board[si][sj] === 'G') return visited[si][sj]
+            if (board[si][sj] === 'G') return cnt
             
             for (const [di, dj] of delta) {
-                let [ni, nj] = [si, sj]
-                if (!board[ni] || !board[ni][nj] || board[ni][nj] === 'D') continue
+                let [ni, nj] = [si+di, sj+dj]
+                if (!board[ni] || !board[ni][nj] || board[ni][nj] === 'D') {
+                    continue
+                }
                 
-                while (board[ni+di] && board[ni+di][nj+dj] && board[ni+di][nj+dj] !== 'D') {
+                while (board[ni] && board[ni][nj] && board[ni][nj] !== 'D') {
                     ni += di
                     nj += dj
                 }
                 
-                if (visited[ni][nj] > visited[si][sj] + 1) {
-                    visited[ni][nj] = visited[si][sj] + 1
-                    que.push([ni, nj])
+                ni -= di; 
+                nj -= dj;
+                if (!visited[ni][nj]) {
+                    visited[ni][nj] = 1
+                    que.push([ni, nj, cnt+1])
                     // console.log('start : ', si, sj)
                     // console.log('arrive :', ni, nj)
                     // console.log('que :', que)
@@ -46,5 +50,3 @@ function solution(board) {
     
     return answer
 }
-
-// 한번에 여러칸을 움직일 수 있기 때문에, 맨 처음 도달하는 횟수가 반드시 최소횟수를 보장하지 않는다.
