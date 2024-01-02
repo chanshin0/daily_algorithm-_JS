71bbd6f4f68d9d509022e4662b0812551c252609
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2024-01-02 22:38:50 +0900

    [level 2] Title: N-Queen, Time: 479.68 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.js
+++ 프로그래머스/2/12952. N－Queen/N－Queen.js
@@ -1,36 +1,28 @@
 function solution(n) {
-    var answer = 0;
-    const visited = Array.from({length:n}, ()=>-1)
-    
-    function dfs(i, q) {
-        // 종료조건
-        if (i === n) {
-            if (q === n) answer ++
-            return
-        }
-        
-        for (let y=0; y<n; y++) {
-            if(!isSafe(i, y)) continue
+    let answer = 0;
+    const queens = [];
 
-            visited[i] = y
-            dfs(i+1, q+1)
-            visited[i] = -1
+    function isPossible(row, i) {
+        for (const [x, y] of queens) {
+            if (row === x || i === y) return false;
+            if (Math.abs(x - row) === Math.abs(y - i)) return false;
         }
-        
-        return
+        return true;
     }
-    
-    function isSafe(i, j) {
-        for (let row=0; row<n; row++) {
-            const col = visited[row]
-            if (col === -1) continue
-            if (col === j) return false
-            if (Math.abs(row-i) === Math.abs(col-j)) return false
+
+    function backtrack(row) {
+        if (row === n) {
+            answer++;
         }
-        return true
+        for (let i = 0; i < n; i++) {
+            if (!isPossible(row, i)) continue;
+            queens.push([row, i]);
+            backtrack(row + 1);
+            queens.pop();
+        }
+
+
     }
-    
-    dfs(0, 0, 0)
-    
+    backtrack(0);
     return answer;
 }
