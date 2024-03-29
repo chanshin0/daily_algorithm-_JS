67216d6e454d9a40ac8ede605ba6a4d3cf68cac4
신동찬 <dongchan@sindongchans-MacBook-Pro.local>
2024-03-29 12:50:04 +0900

    0329 TIL

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ let input = require("fs")
   .readFileSync(filePath)
   .toString()
   .replace(/\r/g, "")
+  .trim()
   .split("\n");
 
-const arr = input.map(Number).sort((a, b) => a - b);
-console.log(arr);
-
-let [s, e] = [0, 6];
-let sum = 0;
-
-const answer = [];
-for (let i = 0; i < 3; i++) {
-
-}
+console.log(input);
