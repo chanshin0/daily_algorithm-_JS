e72251d069449ecf76ee7a9f611009d094cfbc3e
Dongchan Shin <109333130+chanshin0@users.noreply.github.com>
2024-11-02 18:57:34 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 77.56 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,66 +1,73 @@
 function solution(land) {
     var answer = 0;
-    const rowLen = land.length
-    const colLen = land[0].length
-    const visited = Array.from({length:rowLen}, ()=>Array.from({length:colLen}, (_)=>0))
-    const obj = {}
-    const amounts = Array(colLen).fill(0)
-    
+    const rowLen = land.length;
+    const colLen = land[0].length;
+    const visited = Array.from({ length: rowLen }, () =>
+        Array.from({ length: colLen }, () => 0)
+    );
+    const obj = {};
+
     // 전체 돌면서 석유 정보를 obj에 저장
-    let num = 1
-    land.forEach((row, r)=>{
-        row.forEach((col, c)=>{
+    let num = 1;
+    land.forEach((row, r) => {
+        row.forEach((col, c) => {
             if (col === 1 && visited[r][c] === 0) {
-                const [amount, arr] = check(r,c)
-                arr.forEach(v => amounts[v] += amount)
-                num++
+                check(r, c);
+                num++;
             }
-        })
-    })
+        });
+    });
+
     
-    // console.log(amounts)
-    
-//     for (let j=0; j<colLen; j++) {
-//         let num = -1;
-//         let maxV = 0
-//         for (let i=0; i<rowLen; i++) {
-//             if (visited[i][j] === 0) continue
-//             if (visited[i][j] === num) continue
-            
-//             num = visited[i][j]
-//             maxV += obj[num]
-//         }
-        
-//         answer = Math.max(answer, maxV)
-//     }
+    for (let j = 0; j < colLen; j++) {
+        let maxV = 0;
+        let seenGroups = new Set(); // 이미 본 그룹을 저장하는 집합
+        for (let i = 0; i < rowLen; i++) {
+            if (visited[i][j] === 0) continue;
+            const groupNum = visited[i][j];
+            if (seenGroups.has(groupNum)) continue;
+
+            seenGroups.add(groupNum);
+            maxV += obj[groupNum];
+        }
+
+        answer = Math.max(answer, maxV);
+    }
 
     function check(i, j) {
-        const delta = [[0,1],[1,0],[0,-1],[-1,0]]
-        
-        const set = new Set()
-        let cnt = 1
-        const que = [[i, j]]
-        visited[i][j] = num
-        while(que.length) {
-            const [si, sj] = que.pop()
-            set.add(sj)
+        const delta = [
+            [0, 1],
+            [1, 0],
+            [0, -1],
+            [-1, 0],
+        ];
+
+        let cnt = 1;
+        const que = [[i, j]];
+        visited[i][j] = num;
+        while (que.length) {
+            const [si, sj] = que.pop();
 
             for (const [di, dj] of delta) {
-                const [ni, nj] = [si+di, sj+dj]
+                const [ni, nj] = [si + di, sj + dj];
 
-                if (ni >= 0 && ni < rowLen && nj >= 0 && nj < colLen 
-                    && visited[ni][nj] === 0 && land[ni][nj] === 1) {
-                    visited[ni][nj] = num
-                    que.push([ni, nj])
-                    cnt++
+                if (
+                    ni >= 0 &&
+                    ni < rowLen &&
+                    nj >= 0 &&
+                    nj < colLen &&
+                    visited[ni][nj] === 0 &&
+                    land[ni][nj] === 1
+                ) {
+                    visited[ni][nj] = num;
+                    que.push([ni, nj]);
+                    cnt++;
                 }
             }
         }
-        
-        return [cnt, Array.from(set)]
+
+        obj[num] = cnt;
     }
-    
-    return Math.max(...amounts);
+
+    return answer;
 }
-
-
